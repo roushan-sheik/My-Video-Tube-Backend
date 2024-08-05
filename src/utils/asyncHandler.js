e67c14c1bce66e15c40e01a62e77999b28ac949b
roushan-sheik <roushansheik@gmail.com>
2024-08-05 18:44:58 +0600

    added: api error middleware

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,26 @@
-//**NOTE -  Try catch way
-const asyncHandler = (func) => async (req, res, next) => {
-  try {
-    await func(req, res, next);
-  } catch (error) {
-    res.status(error.status).json({
-      success: false,
-      message: error.message,
-    });
-  }
+import { Promise } from "mongoose";
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
 };
 
+
 export { asyncHandler };
+
+//**NOTE -  Try catch way
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.status).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+// export { asyncHandler };
