785d0854345e460b4148e962598a39e0d19762ba
roushan-sheik <roushansheik@gmail.com>
2024-08-05 08:12:28 +0600

    added: Check STATE Database Already Connected Or NOT

--- src/db/index.js
+++ src/db/index.js
@@ -1,23 +1,32 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try
-  {
-    // connection string making 
-    let connectionUri = process.env.MONGO_URI;
-    connectionUri = connectionUri.replace(
-      "<username>",
-      process.env.MONGO_USER_NAME
-    );
-    connectionUri = connectionUri.replace(
-      "<password>",
-      process.env.MONGO_USER_PASSWORD
-    );
-    connectionUri = `${connectionUri}/${process.env.DB_NAME}`;
+  // connection string making
+  let connectionUri = process.env.MONGO_URI;
+  connectionUri = connectionUri.replace(
+    "<username>",
+    process.env.MONGO_USER_NAME
+  );
+  connectionUri = connectionUri.replace(
+    "<password>",
+    process.env.MONGO_USER_PASSWORD
+  );
+  connectionUri = `${connectionUri}/${process.env.DB_NAME}`;
+  try {
+    // Check Connection URI
     if (!connectionUri) {
       throw new Error("MONGO_URI is not defined in .env FILE");
     }
-
+    // Check STATE Database Already Connected Or NOT
+    const connectionState = mongoose.connection.readyState;
+    if (connectionState === 1) {
+      console.log("Database Already Connected");
+      return;
+    }
+    if (connectionState === 2) {
+      console.log("Connecting to the Database...");
+      return;
+    }
     const connectionInstance = await mongoose.connect(connectionUri);
     console.log(
       `\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`
