f1a68c727e5c8de0fa9c69d753be196edcbae30b
roushan-sheik <roushansheik@gmail.com>
2024-08-05 09:32:16 +0600

    added: basic middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 
 const app = express();
+// middleware
+const corseOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+app.use(cors(corseOptions));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
