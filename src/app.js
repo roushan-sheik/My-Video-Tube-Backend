7d784125a5f22259cd9970f9ab15875c1c4570ef
roushan-sheik <roushansheik@gmail.com>
2024-08-05 08:45:34 +0600

    added: basic app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log("Advanced Backend");
+import express from "express";
+
+const app = express();
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // require( "dotenv" ).config( { path: "./env" } );
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
-
 dotenv.config();
 
-connectDB();
+// App listening port
+const PORT = process.env.PORT || 8000;
+// connect to the database
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running on port: http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection FAILED!!", error);
+  });
