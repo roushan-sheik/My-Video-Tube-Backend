421ab4270e6815e0905be88c89fdbc2ca375a927
roushan-sheik <roushansheik@gmail.com>
2024-08-06 17:05:09 +0600

    added: user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // Cloudinary url
+      required: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: true,
+    },
+  },
+  { timestamps: true }
+);
+const User = model("User", userSchema);
