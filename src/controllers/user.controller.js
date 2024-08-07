5f6620fe25220f8e36d7a484a8ba42627b861ab7
roushan-sheik <roushansheik@gmail.com>
2024-08-07 16:55:56 +0600

    added: Get user details from frontend and validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,16 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   //  Write down ths steps
   // 1. Get user details from frontend
+  const { username, fullName, email, password } = req.body;
   // 2. Validation - not empty
+  if (
+    [username, fullName, email, password].some((filed) => filed?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required.");
+  }
   // 3. Check user is already exists: name , email
   // 4. Check for images, check for avatar
   // 5. Upload to them cloudinary, avatar check
@@ -12,7 +19,6 @@ const registerUser = asyncHandler(async (req, res) => {
   // 8. Remove password and refresh token from response
   // 9. Check for user creation
   // 10. Return response
-  const { username, fullName, email, password } = req.body;
 });
 
 export { registerUser };
